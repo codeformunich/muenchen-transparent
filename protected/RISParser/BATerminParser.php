94bd762fd62251f858cc85889170f21643ec2071
Tobias Hößl <tobias@hoessl.eu>
2014-09-17 22:45:28 +0200

    Das Löschen von TOPs schlug fehl

--- protected/RISParser/BATerminParser.php
+++ protected/RISParser/BATerminParser.php
@@ -203,6 +203,11 @@ class BATerminParser extends RISParser
 
 		foreach ($bisherige_tops as $top) if (!in_array($top->top_betreff, $verwendete_top_betreffs)) {
 			$aenderungen_tops .= "TOP entfernt: " . $top->top_betreff . "\n";
+			echo count($top->dokumente);
+			foreach ($top->dokumente as $dok) {
+				$dok->ergebnis_id = null;
+				$dok->save(false);
+			}
 			$top->delete();
 		}
 
