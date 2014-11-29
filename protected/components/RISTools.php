690753a7168dfefe650926414677ed166509dca0
Tobias Hößl <tobias@hoessl.eu>
2014-11-29 21:36:03 +0100

    Zeichenkorrektur verbessern

--- protected/components/RISTools.php
+++ protected/components/RISTools.php
@@ -154,11 +154,13 @@ class RISTools
 	 */
 	public static function korrigiereTitelZeichen($titel)
 	{
+		$titel = trim($titel);
+		$titel = str_replace(" ? ", " — ", $titel);
 		$titel = preg_replace("/([\\s-\(])\?(\\w[^\\?]*[\\w.])\?/siu", "\\1„\\2“", $titel);
+		$titel = preg_replace("/^\?(\\w[^\\?]*[\\w\.])\?/siu", "„\\1“", $titel);
 		$titel = preg_replace("/^\?(\\w[^\\?]*\\w)\?/siu", " „\\1“", $titel);
 		$titel = preg_replace("/([0-9])\?([0-9])/siu", " \\1-\\2", $titel);
 		$titel = preg_replace("/\\s\?$/siu", "?", $titel);
-		$titel = str_replace(" ?", " —", $titel);
 		$titel = str_replace(chr(10) . "?", " —", $titel);
 		$titel = str_replace("Â?", "€", $titel);
 		return $titel;
