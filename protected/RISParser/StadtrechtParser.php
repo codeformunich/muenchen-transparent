19bdd33300edeabfe3046e5087016d17765cebba
konstin <konstin@konstin.no-ip.biz>
2014-11-30 20:38:43 +0100

    Revert "Parser für Stadtrecht vervollständigt" bis das Pull-Request entschieden ist.
    This reverts commit a91f9f2ad5fe96792a28801bb9363590f0e66806.

--- protected/RISParser/StadtrechtParser.php
+++ protected/RISParser/StadtrechtParser.php
@@ -1,30 +1,18 @@
 <?php
 
+
 class StadtrechtParser extends RISParser
 {
-    public function parseIndex() {
-        $all_docs = [];
-        $index    = ris_download_string("http://www.muenchen.info/dir/recht/alph_portal.html");
-        $lines    = explode("\n", $index);
-        foreach ($lines as $line) {
-            if(preg_match("/<td\><a href=\"(\S+)\.htm\" target=\"_blank\">([\S ]+)<\/a><\/td>/i", $line, $matches)) {
-                $kuerzel   = $matches[2];
-                $name      = $matches[2];
-                $base_url  = "http://www.muenchen.info/dir/recht/" . $matches[1];
-                $id        = preg_replace("/\S+\/(\S+)/", "$1", $matches[1]);
-                array_push($all_docs, array($base_url, $id, $kuerzel, $name));
-            }
-        }
-        return $all_docs;
-    }
+
 
     public function parse($id) {
         // @TODO
     }
 
-    public function parseByURL($base_url, $id, $kuerzel, $name) {
-        $full_url = $base_url . "/css/" . $id . "_index.htm";
-        $index  = ris_download_string($full_url);
+    // http://www.muenchen.info/dir/recht/23/23_20100525/css/23_20100525
+    public function parseByURL($url, $url_pdf, $kuerzel, $name) {
+        echo $url . "_index.htm\n";
+        $index  = ris_download_string($url . "_index.htm");
 
         preg_match("/gLastPage = (?<seiten>[0-9]+);/siu", $index, $matches);
         $seiten = $matches["seiten"];
@@ -35,17 +23,16 @@ class StadtrechtParser extends RISParser
 
         for ($seite = 1; $seite <= $seiten; $seite++) {
 
-            $document = ris_download_string($base_url . "/css/" . $id . "_" . $seite . ".htm");
+            $document = ris_download_string($url . "_" . $seite . ".htm");
 
-            // workaround for https://bugs.php.net/bug.php?id=61484
-            ini_set('mbstring.substitute_character', "none");
-            $document= mb_convert_encoding($document, 'UTF-8', 'UTF-8');
+            $document = iconv("UTF8", "UTF8//IGNORE", $document);
 
             $x = explode('!-- text starts here -->', $document);
             $x = explode('</BODY>', $x[1]);
             $text = $x[0];
 
             $html = str_replace(array("<NOBR>", "</NOBR>", "<SPAN", "</SPAN"), array("", "", "<DIV", "</DIV"), $text);
+
 
             preg_match("/text positioning information \*\/\\n(?<css>.*)<\/STYLE/siu", $document, $matches);
             $x = explode('* text positioning information */', $document);
@@ -60,19 +47,20 @@ class StadtrechtParser extends RISParser
             }
 
             $texte .= '<section class="seite seite' . $seite . '">' . $html . '</section>' . "\n\n\n";
+
         }
 
         echo $css;
         /** @var Rechtsdokument $rechtsdokument */
-        $rechtsdokument = Rechtsdokument::model()->findByAttributes(array("url_base" => $base_url . $id));
+        $rechtsdokument = Rechtsdokument::model()->findByAttributes(array("url_base" => $url));
         if (!$rechtsdokument) $rechtsdokument = new Rechtsdokument();
 
-        $rechtsdokument->name     = $name;
-        $rechtsdokument->url_base = $full_url;
-        $rechtsdokument->url_pdf  = $base_url . ".pdf";
-        $rechtsdokument->html     = $texte;
-        $rechtsdokument->css      = $css;
-        $rechtsdokument->nr       = $kuerzel;
+        $rechtsdokument->name = $name;
+        $rechtsdokument->url_base = $url;
+        $rechtsdokument->url_pdf = $url_pdf;
+        $rechtsdokument->html = $texte;
+        $rechtsdokument->css = $css;
+        $rechtsdokument->nr = $kuerzel;
 
         $rechtsdokument->save();
         var_dump($rechtsdokument->getErrors());
@@ -83,19 +71,11 @@ class StadtrechtParser extends RISParser
     }
 
     public function parseAlle() {
-        $all_docs = $this->parseIndex();
-        echo $all_docs[0][0] . " " . $all_docs[0][1] . " " . $all_docs[0][2] . " " . $all_docs[0][3];
-        foreach ($all_docs as $doc) {
-            $this->parseByURL($doc[0], $doc[1], $doc[2], $doc[3]);
-        }
+
     }
 
     public function parseUpdate() {
-        // TODO
-        $all_docs = $this->parseIndex();
-        foreach ($all_docs as $doc) {
-            parseByURL($doc[0], $doc[1], $doc[2], $doc[3]);
-        }
+
     }
 
 }
--- protected/commands/Reindex_RechtCommand.php
+++ protected/commands/Reindex_RechtCommand.php
@@ -4,7 +4,9 @@
 
 class Reindex_RechtCommand extends CConsoleCommand {
     public function run($args) {
+
         $parser = new StadtrechtParser();
-        $parser->parseAlle();
+        $parser->parseByURL("http://www.muenchen.info/dir/recht/1/1_20131017/css/1_20131017", "http://www.muenchen.info/dir/recht/1/1_20131017.pdf", "HauptS", "Hauptsatzung");
+
     }
 }
