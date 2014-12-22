18fd08325c46d4c982be6ec6a43711e29031b958
Tobias Hößl <tobias@hoessl.eu>
2014-12-22 12:20:17 +0100

    jQuery-Update

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -65,10 +65,10 @@
     ?>
 
     <!--[if lt IE 9]>
-    <script src="/js/jquery-1.11.1.min.js"></script>
+    <script src="/js/jquery-1.11.2.min.js"></script>
     <![endif]-->
 	<!--[if gte IE 9]><!-->
-	<script src="/js/jquery-2.1.1.min.js"></script>
+	<script src="/js/jquery-2.1.3.min.js"></script>
 	<!--<![endif]-->
 
 	<!--[if lt IE 9]>
