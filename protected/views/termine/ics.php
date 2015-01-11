805be1be70f4ae84ab297b6007481e7e3f9eceb0
Tobias Hößl <tobias@hoessl.eu>
2015-01-11 09:14:44 +0100

    Einfachere URLs

--- protected/views/termine/ics.php
+++ protected/views/termine/ics.php
@@ -40,7 +40,6 @@ $vcalendar = new \Sabre\VObject\Component\VCalendar();
 
 foreach ($alle_termine as $curr_termin) {
 	$vcalendar->add('VEVENT', $curr_termin->getVEventParams());
-
 }
 
 echo $vcalendar->serialize();
