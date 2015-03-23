6f2cb5c5a9b3c45ad90ab57e4e767028b036d43d
konstin <konstin@konstin.no-ip.biz>
2015-03-23 21:48:13 +0100

    git add vergessen...

---
+++ protected/views/benachrichtigungen/reset_password_done.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* @var IndexController $this
+*/
+
+$this->pageTitle = "Passwort zurückgesetzt";
+?>
+
+<section class="col-md-6 col-md-offset-3">
+    <div class="well">
+        <h1 class="form-signin-heading">Geschafft!</h1>
+        <div class="alert alert-success">
+            Dein Passwort wurde erfolgreich zurückgesetz.
+        </div>
+    </div>
+</section>
---
+++ protected/views/benachrichtigungen/reset_password_set_form.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* @var IndexController $this
+* @var string $current_url
+*/
+$this->pageTitle = "Passwort zurücksetzen";
+?>
+
+
+<section class="col-md-4 col-md-offset-4">
+    <div class="well">
+
+        <form class="form-horizontal form-signin" method="POST" action="<?= CHtml::encode($current_url) ?>">
+            <fieldset>
+                <legend class="form_row">Neues Passwort setzen</legend>
+
+                <div class="form_row">
+                    <label for="password" class="control-label sr-only">Passwort</label>
+                    <input id="password" name="password" type="password" class="form-control" placeholder="Passwort" required autofocus>
+                </div>
+                <div class="form_row">
+                    <label for="password2" class="control-label sr-only">Passwort bestätigen</label>
+                    <input id="password2" name="password2" type="password" class="form-control" placeholder="Passwort bestätigen" required>
+                </div>
+
+                <button class="btn btn-lg btn-primary btn-block" type="submit" name="<?php echo AntiXSS::createToken("reset_password"); ?>">Setzen</button>
+            </fieldset>
+        </form>
+    </div>
+</section>
