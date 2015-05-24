e41fa09aed855e256e96bdc87b1428fbeb3e6eb9
konstin <konstin@konstin.no-ip.biz>
2015-05-24 16:41:35 +0200

    Schriftgröße angepasst

--- protected/views/index/index_antraege_liste.php
+++ protected/views/index/index_antraege_liste.php
@@ -141,7 +141,7 @@ if (count($antraege) > 0) {
 
             $titel = $entry->getName(true);
             echo '<li>
-            <div><a href="' . CHtml::encode($entry->getLink()) . '"';
+            <div><a class="antragstitel" href="' . CHtml::encode($entry->getLink()) . '"';
             if (mb_strlen($titel) > 110) echo ' title="' . CHtml::encode($titel) . '"';
             echo '><span>';
             echo CHtml::encode($titel) . '</span></a></div>';
