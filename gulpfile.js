bd46c00bf167ed8045f09d271df90b520845b15f
konstin <konstin@wmail.io>
2016-01-01 20:14:22 +0100

    gulpfile auf reine tasks umgestellt

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,9 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglifyjs'),
     concat = require('gulp-concat'),
-    watch = require('gulp-watch');
     gutil = require('gulp-util');
 
-function build_js() {
+gulp.task('std.js', function () {
     gulp.src(["./html/js/jquery-ui-1.11.2.custom.min.js", "./html/js/scrollintoview.js", "./html/js/antraegekarte.jquery.js",
             "./html/js/bootstrap.min.js", "./html/js/material/ripples.min.js", "./html/js/material/material.min.js",
             "./html/js/typeahead.js/typeahead.bundle.min.js", "./html/js/index.js"])
@@ -15,7 +14,9 @@ function build_js() {
         .pipe(uglify())
         .pipe(gulp.dest('./html/js/build/'))
         .on('end', function(){ gutil.log('main js finished'); });
+});
 
+gulp.task('leaflet.js', function () {
     gulp.src(["./html/js/leaflet/dist/leaflet.js", "./html/js/Leaflet.Fullscreen/Control.FullScreen.js",
             "./html/js/Leaflet.Control.Geocoder/Control.Geocoder.js", "./html/js/Leaflet.draw-0.2.3/dist/leaflet.draw.js",
             "./html/js/leaflet.spiderfy.js", "./html/js/leaflet.textmarkers.js", "./html/js/leaflet.locatecontrol/dist/L.Control.Locate.min.js"])
@@ -24,9 +25,9 @@ function build_js() {
         .pipe(uglify())
         .pipe(gulp.dest('./html/js/build/'))
         .on('end', function(){ gutil.log('leaflet finished'); });
-}
+});
 
-function build_sass() {
+gulp.task('sass', function () {
     gulp.src('./html/css/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -35,19 +36,11 @@ function build_sass() {
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./html/css'))
         .on('end', function(){ gutil.log('css finished'); });
-}
-
-gulp.task('default', function () {
-    build_js();
-    build_sass();
 });
 
-gulp.task('watch', function () {
-    watch('./html/js/**/*.js', {interval: 500}, function () {
-        build_js();
-    });
+gulp.task('default', ['std.js', 'leaflet.js', 'sass']);
 
-    watch('./html/css/*.scss', {interval: 500}, function () {
-        build_sass();
-    });
+gulp.task('watch', function () {
+    gulp.watch('./html/js/**/*.js', ['std.js', 'leaflet.js']);
+    gulp.watch('./html/css/*.scss', ['sass']);
 });
