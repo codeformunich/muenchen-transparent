9d2909daead46d70bcfcf37793c6ede70ae1528e
konstin <konstin@wmail.io>
2016-01-18 21:09:07 +0100

    Codeception in travis mit nginx und php5-fpm, 1

--- tests/acceptance/LoginCept.php
+++ tests/acceptance/LoginCept.php
@@ -4,7 +4,5 @@ $I->wantTo('log in');
 $I->amOnPage('/benachrichtigungen');
 $I->fillField('email', 'user@example.com');
 $I->fillField('password', '1234');
-//$I->fillField('#login', '');
-//$I->click('#login');
 $I->submitForm('#login', [], $I->grabAttributeFrom('#login', 'name'));
 $I->see('Benachrichtigungen an user@example.com:');
