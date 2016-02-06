717173dd0ba5d74e254989d07d4ff469ff24688c
konstin <konstin@mailbox.org>
2016-02-06 13:05:41 +0100

    Login-Test an die neuen Versionen anpassen

--- tests/acceptance/LoginCept.php
+++ tests/acceptance/LoginCept.php
@@ -4,5 +4,6 @@ $I->wantTo('log in');
 $I->amOnPage('/benachrichtigungen');
 $I->fillField('email', 'user@example.com');
 $I->fillField('password', '1234');
-$I->submitForm('#login', [], $I->grabAttributeFrom('#login', 'name'));
+// TODO: write a helper for the next line when there are more tests working through XSS protection
+$I->submitForm('#login', [$I->grabAttributeFrom('#login', 'name') => ""]); 
 $I->see('Benachrichtigungen an user@example.com:');
