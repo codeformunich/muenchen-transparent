b0c0f5d3c76745d36696951513931e03264a6d49
Tobias Hößl <tobias@hoessl.eu>
2016-02-15 20:31:17 +0100

    remove obsolete Twitter Share Count API

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -845,7 +845,7 @@ class IndexController extends RISBaseController
         Header("Content-Type: application/json; charset=UTF-8");
         $shariff = new \Heise\Shariff\Backend([
             "domain"   => $_SERVER["HTTP_HOST"],
-            "services" => ["Facebook", "Twitter", "GooglePlus"],
+            "services" => ["Facebook", "GooglePlus"],
             "cache"    => [
                 "ttl"      => 60,
                 "cacheDir" => TMP_PATH,
