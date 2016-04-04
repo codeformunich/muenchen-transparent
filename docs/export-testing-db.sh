1cd8e65a03cf8a0eaa2c5467ff00c3e1917e11db
konstin <konstin@mailbox.org>
2016-04-04 21:49:58 +0200

    Script zum Exportieren der Test-Datenbank hinzugefügt

---
+++ docs/export-testing-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Updates the data dump used for the tests
+
+# cd into the git root
+cd $(git rev-parse --show-toplevel)
+
+configfile="protected/config/main-codeception.php"
+
+# Extract the db connection information from the config file using regex
+db=$(      sed -ne "s/ *'connectionString'      => 'mysql:host=127.0.0.1;dbname=\(.*\)',.*/\1/p" ${configfile})
+username=$(sed -ne "s/ *'username'              => '\(.*\)',.*/\1/p"                            ${configfile})
+password=$(sed -ne "s/ *'password'              => '\(.*\)',.*/\1/p"                            ${configfile})
+
+# Only use the password option if a password has been specified
+if [ -n "${password}" ]; then
+    password="-p${password}"
+fi
+
+mysqldump --skip-opt --single-transaction --disable-keys --no-create-info -u${username} ${password} ${db} > tests/_data/data.sql
