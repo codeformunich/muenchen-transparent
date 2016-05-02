570adf48e001ac2c3aaebde2c0360ababe59ca11
konstin <konstin@mailbox.org>
2016-05-02 20:46:18 +0200

    fix oparl test case

--- tests/oparl/object/FileCept.php
+++ tests/oparl/object/FileCept.php
@@ -7,7 +7,7 @@ $I->seeOparl('
   "id": "http://localhost:8080/oparl/v1.0/file/7",
   "type": "https://oparl.org/schema/1.0/File",
   "name": "Dokument mit vielen Eigenschaften",
-  "muenchenTransparent:orignalAccessUrl": "http://www.ris-muenchen.de/RII/RII/7.pdf",
+  "muenchenTransparent:orignalAccessUrl": "https://www.ris-muenchen.de/RII/RII/7.pdf",
   "fileName": "Dokument mit vielen Eigenschaften.pdf",
   "mimeType": "application/pdf",
   "accessUrl": "http://localhost:8080/dokumente/7.pdf",
@@ -26,7 +26,7 @@ $I->seeOparl('
   "id": "http://localhost:8080/oparl/v1.0/file/8",
   "type": "https://oparl.org/schema/1.0/File",
   "name": "Dokument (tiff) mit wenig Eigenschaften",
-  "muenchenTransparent:orignalAccessUrl": "http://www.ris-muenchen.de/RII/RII/8.tiff",
+  "muenchenTransparent:orignalAccessUrl": "https://www.ris-muenchen.de/RII/RII/8.tiff",
   "fileName": "Dokument (tiff) mit wenig Eigenschaften.tiff",
   "mimeType": "image/tiff",
   "accessUrl": "http://localhost:8080/dokumente/8.tiff"
@@ -38,7 +38,7 @@ $I->seeOparl('
   "id": "http://localhost:8080/oparl/v1.0/file/9",
   "type": "https://oparl.org/schema/1.0/File",
   "name": "Rathausumschau",
-  "muenchenTransparent:orignalAccessUrl": "http://www.ris-muenchen.dehttp://example.org/rathausumschau/1-rathaus.pdf",
+  "muenchenTransparent:orignalAccessUrl": "https://www.ris-muenchen.dehttp://example.org/rathausumschau/1-rathaus.pdf",
   "fileName": "Rathausumschau.pdf",
   "mimeType": "application/pdf",
   "accessUrl": "http://localhost:8080/dokumente/9.pdf"
