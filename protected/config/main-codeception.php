05871a2cfce8ad3b6fb616d63b661ebb52c582bc
konstin <konstin@mailbox.org>
2016-06-14 20:47:22 +0200

    config-Dateien besser strukturieren

--- protected/config/main-codeception.php
+++ protected/config/main-codeception.php
@@ -23,7 +23,7 @@ define("OMNIPAGE_PDF_DIR", RIS_OMNIPAGE_DIR . "ocr-todo/");
 define("OMNIPAGE_DST_DIR", RIS_OMNIPAGE_DIR . "ocr-dst/");
 define("OMNIPAGE_IMPORTED_DIR", RIS_OMNIPAGE_DIR . "ocr-imported/");
 define("TILE_CACHE_DIR", RIS_DATA_DIR . "tile-cache/tiles/");
-define("EMAIL_LOG_FILE", "/tmp/email.log");
+define("EMAIL_LOG_FILE", TMP_PATH . "/email.log");
 
 // Konstanten, die das RIS betreffen
 define("RIS_URL_PREFIX", "http://localhost:8080"); // Zum Testen des Proxys
@@ -32,9 +32,6 @@ define("RIS_BA_BASE_URL",  "https://www.ris-muenchen.de/RII/BA-RII/");
 define("RATHAUSUMSCHAU_WEBSITE",  "http://www.muenchen.de/rathaus/Stadtinfos/Presse-Service.html");
 define("OPARL_10_ROOT", SITE_BASE_URL . '/oparl/v1.0');
 
-define("DOCUMENT_DATE_ACCURATE_SINCE", 1388530800); // 1. Januar 2014
-define("DOCUMENT_DATE_UNKNOWN_BEFORE", 1212271200); // 1. Juni 2008
-
 define("OPARL_10_ITEMS_PER_PAGE", 3); // Macht das Testen einfacher
 
 define("SEED_KEY", "RANDOMKEY");
@@ -42,15 +39,7 @@ define("MAILGUN_API_KEY", "");
 define("MAILGUN_DOMAIN", "");
 define("NO_ERROR_MAIL", true);
 
-ini_set("memory_limit", "256M");
-mb_internal_encoding("UTF-8");
-mb_regex_encoding("UTF-8");
-define("DEFAULT_TIMEZONE", "Europe/Berlin");
-date_default_timezone_set(DEFAULT_TIMEZONE);
-ini_set('mbstring.substitute_character', "none");
-setlocale(LC_TIME, "de_DE.UTF-8");
-setlocale(LC_NUMERIC, "C"); // Scheint in manchen Umgebungen (HHVM?) sonst bei de_DE Probleme mit FloatVal zu machen
-
+require_once(__DIR__ . "/constants.php");
 require_once(__DIR__ . "/urls.php");
 
 $GLOBALS["SOLR_CONFIG"] = null;
