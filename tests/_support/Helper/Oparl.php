98b0e0d81f05516cb105a6947270a29de8e2dc0c
konstin <konstin@mailbox.org>
2016-06-18 14:30:23 +0200

    [OParl] Alle erwartetes json in einer Datei speichern

--- tests/_support/Helper/Oparl.php
+++ tests/_support/Helper/Oparl.php
@@ -1,6 +1,8 @@
 <?php
 namespace Helper;
 
+use Codeception\Lib\ModuleContainer;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
@@ -14,16 +16,18 @@ class Oparl extends \Codeception\Module
     private $prettyResponse;
     private $tree;
 
-    // really prrivate
+    // really private
     /** The path to the file with the expected response */
-    private $filepath;
+    private $url;
     /** Flag to notify the user about `--env updatejson` in the right situation */
     private $notify_about_updatejson;
     /** Stores all URLs that have already gotten the basic checks to avoid redundant work */
     private $checked_urls = [];
+    /** Stores the expected results in the format ["[url-affix]" => "expected json", ....] */
+    private $expectedResults = [];
 
     /**
-     * Returns the response as pretty pretty-printed json including decoded umlauts
+     * Returns the server response as pretty-printed json including decoded umlauts
      *
      * @return string
      */
@@ -31,13 +35,20 @@ class Oparl extends \Codeception\Module
         return $this->prettyResponse;
     }
 
-    /*
+    /**
+     * Returns the server response as compressed json including decoded umlauts
      *
+     * @return string
      */
     public function getUglyResponse() {
         return $this->uglyResponse;
     }
 
+    /**
+     * Returns the server response as array-based tree
+     *
+     * @return array
+     */
     public function getTree() {
         return $this->tree;
     }
@@ -51,19 +62,41 @@ class Oparl extends \Codeception\Module
         $output->writeln($text);
     }
 
+    public function _beforeSuite() {
+        $filename = codecept_data_dir() . "oparl_expected_results.txt";
+        if (file_exists($filename)) {
+            $content = file_get_contents($filename);
+            preg_match_all('/^([^ ]+) (.+)$/m', $content, $matches, PREG_SET_ORDER);
+            foreach ($matches as $match) {
+                $pretty_expected = stripslashes(json_encode(json_decode($match[2]), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+                $this->expectedResults[$match[1]] = $pretty_expected;
+            }
+        } else {
+            if ($this->config['updatejson'] === true) {
+                $this->writeln("\nCreating missing file with expected response ...");
+                file_put_contents($filename, '\n');
+            } else {
+                $this->fail('The file with expected results is missing (' . $filename . ')');
+            }
+        }
+    }
+
+    public function _afterSuite() {
+        $filename = codecept_data_dir() . "oparl_expected_results.txt";
+        file_put_contents($filename, '');
+        foreach ($this->expectedResults as $url => $pretty_expected) {
+            $ugly_expected = stripslashes(json_encode(json_decode($pretty_expected), JSON_UNESCAPED_UNICODE));
+            file_put_contents($filename, $url . ' ' . $ugly_expected . "\n", FILE_APPEND);
+        }
+    }
 
     /**
      * Sets filepath, uglyResponse, prettyResponse and tree
      */
     public function setVariables($url) {
         $this->checked_urls[] = $url;
-        
-        // edge case of the oparl:system object
-        if ($url == "/") {
-            $url = "/system/0";
-        }
 
-        $this->filepath = codecept_data_dir() . 'oparl' . $url . '.json';
+        $this->url = $url;
         $this->uglyResponse = stripslashes(json_encode(json_decode($this->getModule('REST')->grabResponse()), JSON_UNESCAPED_UNICODE));
         $this->prettyResponse = stripslashes(json_encode(json_decode($this->getModule('REST')->grabResponse()), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         $this->tree = json_decode($this->getUglyResponse());
@@ -76,29 +109,25 @@ class Oparl extends \Codeception\Module
      */
     public function seeOParlFile() {
         $this->notify_about_updatejson = true;
-        // Check if an expected result exists
-        if (!file_exists($this->filepath)) {
-            if ($this->config['updatejson'] === true) {
-                $this->writeln("\nCreating missing file with expected response ...");
-                if (!file_exists(dirname($this->filepath)))
-                    mkdir(dirname($this->filepath), 0777, true);
-                file_put_contents($this->filepath, $this->prettyResponse);
+
+        if ($this->config['updatejson'] === true) {
+            if (!array_key_exists($this->url, $this->expectedResults)) {
+                $this->writeln("\nCreating expected response ...");
+            } else if ($this->expectedResults[$this->url] != $this->getPrettyResponse()) {
+                $this->writeln("\nUpdating expected response ...");
             } else {
-                $this->fail('The file with expected json missing in validateResponse(): ' . $this->filepath);
+                return;
             }
+
+            $this->expectedResults[$this->url] = $this->getPrettyResponse();
         }
 
-        // Finally, check if the response matches
-        $expected = file_get_contents($this->filepath);
-        if ($this->config['updatejson'] !== true) {
-            
-            $this->assertEquals($expected, $this->prettyResponse);
-        } else {
-            if ($this->prettyResponse != $expected) {
-                $this->writeln("\nTest failed. Updating expected JSON ...");
-                file_put_contents($this->filepath, $this->prettyResponse);
-            }
+        if (!array_key_exists($this->url, $this->expectedResults)) {
+            $this->fail('There\'s no expected response for this url: ' . $this->url);
         }
+
+        $this->assertEquals($this->expectedResults[$this->url], $this->getPrettyResponse());
+
         $this->notify_about_updatejson = false;
     }
 
@@ -117,7 +146,7 @@ class Oparl extends \Codeception\Module
      * Prints some usefull debug information about a failed test
      */
     public function _failed(\Codeception\TestCase $test, $fail) {
-        $this->writeln($this->prettyResponse);
+        $this->writeln($this->getPrettyResponse());
         if ($this->notify_about_updatejson) {
             $this->writeln("The file with expected json is missing or differs from the real output.");
             $this->writeln("Run codeception with `--env updatejson` to fix this.");
