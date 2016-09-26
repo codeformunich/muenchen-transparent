cd9eaacfd4cb19ccddd0a8ccf4df45e00f8a1884
konstin <konstin@mailbox.org>
2016-09-26 17:27:42 +0200

    OParl: Test für `gremium == null`

--- tests/oparl/list/MeetingListCept.php
+++ tests/oparl/list/MeetingListCept.php
@@ -2,5 +2,10 @@
 $I = new OparlTester($scenario);
 $I->wantTo('validate the external list oparl:meeting');
 $I->getOParl('/body/0/list/meeting');
-$I->getOParl('/body/0/list/meeting?id=3');
 $I->getOParl('/body/1/list/meeting');
+
+// Check that objects with no organization do not get printed
+$I->getOParl('/body/0/list/meeting?id=3');
+foreach ($I->getResponseAsTree()->data as $object) {
+    $I->assertNotEquals($object->meetingState, "Dateninkonsitent");
+}
