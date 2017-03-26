1f035e5efe7b01b524cc9fac97b0551edb19e547
konstin <konstin@mailbox.org>
2017-03-26 15:52:05 +0200

    [OParl] Fix file access urls

--- tests/oparl/object/FileCept.php
+++ tests/oparl/object/FileCept.php
@@ -2,5 +2,15 @@
 $I = new OparlTester($scenario);
 $I->wantTo('validate oparl:file objects (one with many attributes, one with few attributes and one Rathausumschau)');
 $I->getOParl('/file/7');
+
+$I->amOnPage($I->getResponseAsTree()->accessUrl);
+$I->seeResponseCodeIs(200);
+$I->seeHttpHeader('Content-Type', 'application/pdf');
+$I->dontSeeHttpHeader('Content-Disposition', 'attachment; filename="7 - Dokument (pdf) mit vielen Eigenschaften.pdf"');
+
+$I->amOnPage($I->getResponseAsTree()->downloadUrl);
+$I->seeResponseCodeIs(200);
+$I->seeHttpHeader('Content-Type', 'application/pdf');
+$I->seeHttpHeader('Content-Disposition', 'attachment; filename="7 - Dokument (pdf) mit vielen Eigenschaften.pdf"');
+
 $I->getOParl('/file/8');
-$I->getOParl('/file/9');
