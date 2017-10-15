060b17fb378a9c1e37e74fd7812c9f43dd8afbb6
Tobias Hößl <tobias@hoessl.eu>
2017-10-15 12:55:57 +0200

    Speziallfall für FDP/HUT entfernen

--- protected/models/Fraktion.php
+++ protected/models/Fraktion.php
@@ -103,7 +103,6 @@ class Fraktion extends CActiveRecord implements IRISItem
         if ($name == "&nbsp;" || trim($name) == "") return "fraktionslos";
         if ($kurzfassung) {
             if (in_array($this->id, [3339564, 2988265, 3312425])) return "Bürgerliche Mitte";
-            if ($this->id == 3312427) return "Freiheitsrechte Transparenz Bürgerbeteiligung";
             if (in_array($this->id, [3312426, 1431959, 33])) return "Die Grünen / RL";
         }
         return $name;
