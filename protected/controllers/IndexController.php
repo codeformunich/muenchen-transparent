7178cb28b0b7f9967096504dd3d4c2dd508250eb
Tobias Hößl <tobias@hoessl.eu>
2020-04-19 17:10:35 +0200

    Add CORS headers

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -567,6 +567,12 @@ class IndexController extends RISBaseController
         }
 
         Header("Content-Type: application/json; charset=UTF-8");
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET');
+        header('Access-Control-Max-Age: 100000');
+        header('Access-Control-Allow-Headers: *');
+        Header('Allow: GET');
+
         echo json_encode([
             "type"     => "FeatureCollection",
             "features" => $geojson,
